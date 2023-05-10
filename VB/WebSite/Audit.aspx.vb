65d54356e8212463071b3455516c929d1ca96423
DevExpressExampleBot <fakeEmail>
2023-05-10 15:30:30 +0400

    Source auto update [skip ci]

--- VB/WebSite/Audit.aspx.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System
-
-Partial Public Class Audit
-    Inherits System.Web.UI.Page
-
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-        source.Session = XpoHelper.GetNewSession()
-    End Sub
-End Class
